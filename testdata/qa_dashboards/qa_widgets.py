f15e7b8bdba60288f0c862c0c36e84e40f56ffd3
Martin Matusiak <numerodix@gmail.com>
2021-08-12 11:13:38 +1000

    expand test to cover all widgets

--- testdata/qa_dashboards/qa_widgets.py
+++ testdata/qa_dashboards/qa_widgets.py
@@ -1,17 +1,27 @@
 from typing import List
 
 from libddog.dashboards import (
+    BackgroundColor,
+    Comparator,
+    ConditionalFormat,
+    ConditionalFormatPalette,
     Dashboard,
     DisplayType,
+    Formula,
+    Group,
     LegendColumn,
     LegendLayout,
     LineMarker,
     LineType,
     LineWidth,
+    LiveSpan,
     MarkerSeverity,
+    Note,
+    NotePreset,
     Palette,
     PopulatedTemplateVariable,
     Position,
+    QueryValue,
     RangeMarker,
     Request,
     Scale,
@@ -19,10 +29,13 @@ from libddog.dashboards import (
     Style,
     TemplateVariableDefinition,
     TemplateVariablesPreset,
+    TextAlign,
+    TickEdge,
+    Time,
     Timeseries,
+    Widget,
     YAxis,
 )
-from libddog.dashboards.components import RangeMarker
 from libddog.metrics import (
     AggFunc,
     Aggregation,
@@ -63,8 +76,8 @@ def get_region_tmpl_var_presets() -> List[TemplateVariablesPreset]:
     return presets
 
 
-def get_dashboard() -> Dashboard:
-    query_ec2_cpu = Query(
+def get_timeseries() -> Widget:
+    query = Query(
         metric=Metric(name="aws.ec2.cpuutilization"),
         filter=Filter(conds=[TmplVar(tvar="region")]),
         agg=Aggregation(func=AggFunc.AVG, by=By(tags=["availability-zone"])),
@@ -72,13 +85,11 @@ def get_dashboard() -> Dashboard:
     )
 
     # Exercises most of the properties of a Timeseries
-    wid_ec2_cpu = Timeseries(
+    widget = Timeseries(
         title="EC2: average CPU utilization per az",
-        size=Size(height=4, width=6),
-        pos=Position(x=0, y=0),
         requests=[
             Request(
-                queries=[query_ec2_cpu],
+                queries=[query],
                 display_type=DisplayType.AREAS,
                 style=Style(
                     line_type=LineType.DASHED,
@@ -96,19 +107,139 @@ def get_dashboard() -> Dashboard:
         ],
         legend_layout=LegendLayout.EXPANDED,
         legend_columns=[LegendColumn.AVG, LegendColumn.MAX, LegendColumn.VALUE],
+        size=Size(height=4, width=6),
+        pos=Position(x=0, y=0),
     )
+
+    return widget
+
+
+def get_query_values() -> List[Widget]:
+    query_all_reqs = Query(
+        metric=Metric(name="aws.elb.request_count"),
+        filter=Filter(conds=[TmplVar(tvar="region")]),
+        agg=Aggregation(func=AggFunc.SUM),
+        funcs=[Fill(func=FillFunc.LINEAR), Rollup(func=RollupFunc.SUM, period_s=60)],
+        name="reqs_all",
+    )
+    query_4xx = Query(
+        metric=Metric(name="aws.elb.httpcode_elb_4xx"),
+        filter=Filter(conds=[TmplVar(tvar="region")]),
+        agg=Aggregation(func=AggFunc.SUM),
+        funcs=[Fill(func=FillFunc.LINEAR), Rollup(func=RollupFunc.SUM, period_s=60)],
+        name="reqs_4xx",
+    )
+    query_5xx = Query(
+        metric=Metric(name="aws.elb.httpcode_elb_5xx"),
+        filter=Filter(conds=[TmplVar(tvar="region")]),
+        agg=Aggregation(func=AggFunc.SUM),
+        funcs=[Fill(func=FillFunc.LINEAR), Rollup(func=RollupFunc.SUM, period_s=60)],
+        name="reqs_5xx",
+    )
+
+    widgets: List[Widget] = []
+    x = 6
+
+    for name, query in (("4xx", query_4xx), ("5xx", query_5xx)):
+        widget = QueryValue(
+            title=f"ELB {name.upper()}",
+            time=Time(live_span=LiveSpan.LAST_1W),
+            custom_unit="%",
+            precision=1,
+            requests=[
+                Request(
+                    formulas=[Formula(text=f"100 * (reqs_{name} / reqs_all)")],
+                    queries=[query_all_reqs, query],
+                    conditional_formats=[
+                        ConditionalFormat(
+                            comparator=Comparator.LT,
+                            value=1,
+                            palette=ConditionalFormatPalette.WHITE_ON_GREEN,
+                        ),
+                        ConditionalFormat(
+                            comparator=Comparator.LT,
+                            value=5,
+                            palette=ConditionalFormatPalette.WHITE_ON_YELLOW,
+                        ),
+                        ConditionalFormat(
+                            comparator=Comparator.GTE,
+                            value=5,
+                            palette=ConditionalFormatPalette.WHITE_ON_RED,
+                        ),
+                    ],
+                ),
+            ],
+            size=Size(height=2, width=3),
+            pos=Position(x=x, y=0),
+        )
+        x += 3
+        widgets.append(widget)
+
+    return widgets
+
+
+def get_notes() -> List[Widget]:
+    note_timeseries = Note(
+        preset=NotePreset.ANNOTATION,
+        content=(
+            "**Timeseries** is the most general purpose widget that applies "
+            "to virtually any use case."
+        ),
+        font_size=14,
+        background_color=BackgroundColor.YELLOW,
+        text_align=TextAlign.LEFT,
+        tick_edge=TickEdge.LEFT,
+        pos=Position(x=6, y=2),
+        size=Size(width=3, height=2),
+    )
+
+    note_query_value = Note(
+        preset=NotePreset.ANNOTATION,
+        content=(
+            "**QueryValue** is a widget often used on roll up dashboards to "
+            "show a summary of a metric over time."
+        ),
+        font_size=14,
+        background_color=BackgroundColor.PURPLE,
+        text_align=TextAlign.LEFT,
+        tick_edge=TickEdge.TOP,
+        pos=Position(x=9, y=2),
+        size=Size(width=3, height=2),
+    )
+
+    return [note_timeseries, note_query_value]
+
+
+def get_group() -> Widget:
+    wid_ec2_cpu = get_timeseries()
+    wids_5xx_perc = get_query_values()
+    wids_notes = get_notes()
+    widgets: List[Widget] = [wid_ec2_cpu] + wids_5xx_perc + wids_notes
+
+    group = Group(
+        title="Exercise all supported widgets",
+        background_color=BackgroundColor.VIVID_ORANGE,
+        widgets=widgets,
+    )
+
+    return group
+
+
+def get_dashboard() -> Dashboard:
+    group = get_group()
 
     tmpl_presets_region = get_region_tmpl_var_presets()
     dashboard = Dashboard(
         title="libddog QA: exercise widgets",
         desc=(
             "This dashboard is used exclusively for the purposes of integration "
-            "testing **libddog**, which is a Datadog automation tool.\n\n"
+            "testing **libddog**, which is an open source Datadog automation "
+            "tool created by Nearmap.\n\n"
             "It is used during automated test runs as well as for manual (visual) "
             "inspection that everything looks correct.\n\n"
             "Get libddog on [Github](https://github.com/nearmap/libddog)."
         ),
-        widgets=[wid_ec2_cpu],
+        widgets=[group],
         tmpl_var_presets=tmpl_presets_region,
     )
 
