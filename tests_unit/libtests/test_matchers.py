e8fc6e5076fbf53d42e3a2aff944bb63c0ce9314
Martin Matusiak <numerodix@gmail.com>
2021-08-13 11:36:21 +1000

    wip matcher

--- tests_unit/libtests/test_matchers.py
+++ tests_unit/libtests/test_matchers.py
@@ -4,38 +4,38 @@ from libtests.matchers import obj_matcher
 
 
 def test__single_level_dict__noop() -> None:
-    fst = {"id": 123, "first_name": "James", "last_name": "Joy"}
-    snd = {"id": 123, "first_name": "James", "last_name": "Joy"}
+    fst = {"id": 123, "first-name": "James", "last-name": "Joy"}
+    snd = {"id": 123, "first-name": "James", "last-name": "Joy"}
 
     assert fst == obj_matcher(snd, {})
 
 
 def test__single_level_dict__single_assignment() -> None:
-    fst = {"id": 123, "first_name": "James", "last_name": "Joy"}
-    snd = {"id": 123, "first_name": "Bill", "last_name": "Joy"}
+    fst = {"id": 123, "first-name": "James", "last-name": "Joy"}
+    snd = {"id": 123, "first-name": "Bill", "last-name": "Joy"}
 
-    assert fst == obj_matcher(snd, {"= /first_name": "James"})
+    assert fst == obj_matcher(snd, {"= /first-name": "James"})
 
 
 def test__single_level_dict__multiple_assignments() -> None:
-    fst = {"id": 123, "first_name": "James", "last_name": "Joy"}
-    snd = {"id": 456, "first_name": "Bill", "last_name": "Joy"}
+    fst = {"id": 123, "first-name": "James", "last-name": "Joy"}
+    snd = {"id": 456, "first-name": "Bill", "last-name": "Joy"}
 
-    assert fst == obj_matcher(snd, {"= /id": 123, "= /first_name": "James"})
+    assert fst == obj_matcher(snd, {"= /id": 123, "= /first-name": "James"})
 
 
 def test__single_level_dict__single_delete() -> None:
-    fst = {"id": 123, "last_name": "Joy"}
-    snd = {"id": 123, "first_name": "Bill", "last_name": "Joy"}
+    fst = {"id": 123, "last-name": "Joy"}
+    snd = {"id": 123, "first-name": "Bill", "last-name": "Joy"}
 
-    assert fst == obj_matcher(snd, {"- /first_name": None})
+    assert fst == obj_matcher(snd, {"- /first-name": None})
 
 
 def test__single_level_dict__assign_and_delete() -> None:
-    fst = {"id": 123, "last_name": "Joy"}
-    snd = {"id": 456, "first_name": "Bill", "last_name": "Joy"}
+    fst = {"id": 123, "last-name": "Joy"}
+    snd = {"id": 456, "first-name": "Bill", "last-name": "Joy"}
 
-    assert fst == obj_matcher(snd, {"= /id": 123, "- /first_name": None})
+    assert fst == obj_matcher(snd, {"= /id": 123, "- /first-name": None})
 
 
 # single level list
