ed4ebb2dca9ab933413dcb02f0d09274895cb650
Martin Matusiak <numerodix@gmail.com>
2021-08-13 11:35:24 +1000

    wip matcher

--- libtests/matchers.py
+++ libtests/matchers.py
@@ -1,4 +1,5 @@
-from typing import Any, Dict
+import enum
+from typing import Any, Dict, List
 
 from libddog.common.types import JsonDict
 
@@ -19,9 +20,101 @@ def dict_matcher(dct: JsonDict, *args: Dict[Any, Any], **patches: str) -> JsonDi
     return dct
 
 
-class DictComparator:
-    def __init__(self) -> None:
-        pass
+class PatchInstructionDidNotMatchAnything(Exception):
+    pass
 
-    def cmp(self, expected: JsonDict, actual: JsonDict) -> None:
-        pass
+
+class PatchOp(enum.Enum):
+    NOOP = 0
+    SET = 1
+    DELETE = 2
+
+
+class PatchInstruction:
+    def __init__(self, path: str, value: str, op: PatchOp) -> None:
+        self.path = path
+        self.value = value
+        self.op = op
+
+    def maybe_apply(self, path: str) -> Any:
+        if self.path == path:
+            if self.op is PatchOp.SET:
+                return self.op, self.value
+            elif self.op is PatchOp.DELETE:
+                return self.op, None
+
+        return PatchOp.NOOP, None
+
+
+class ObjectPatcher:
+    def __init__(self, instructions: List[PatchInstruction]) -> None:
+        self.instructions = instructions
+
+    @classmethod
+    def create(cls, patches: Dict[str, Any]) -> "ObjectPatcher":
+        instructions = []
+
+        for key, value in patches.items():
+            op_elem, _, path = key.partition(" ")
+
+            op = None
+            if op_elem == "-":
+                op = PatchOp.DELETE
+            elif op_elem == "=":
+                op = PatchOp.SET
+            else:
+                raise ValueError("Unsupported op: %s" % op_elem)
+
+            if not path.startswith("/"):
+                raise ValueError("Invalid patch path: %s" % path)
+
+            instruction = PatchInstruction(path=path, value=value, op=op)
+            instructions.append(instruction)
+            # print(instruction.__dict__)
+
+        return cls(instructions=instructions)
+
+    def apply_all_to_dict(self, path: str, dct: Dict[str, Any], key: str) -> None:
+        for instruction in self.instructions:
+            op, new_value = instruction.maybe_apply(path)
+            if op is PatchOp.SET:
+                dct[key] = new_value
+            elif op is PatchOp.DELETE:
+                del dct[key]
+
+    def apply_all_to_list(self, path: str, lst: List[Any], idx: int) -> None:
+        for instruction in self.instructions:
+            op, new_value = instruction.maybe_apply(path)
+            if op is PatchOp.SET:
+                lst[idx] = new_value
+            elif op is PatchOp.DELETE:
+                del lst[idx]
+
+    def rewrite(self, obj: object, path="/") -> Any:
+        # import pdb; pdb.set_trace()
+        if isinstance(obj, dict):
+            orig_dict = obj
+            cloned_dict = dict(obj)
+
+            # can't modify the dict while iterating over it, so iterate over the
+            # original and modify the clone
+            for key in orig_dict.keys():
+                nested_path = f"{path}{key}"
+                self.apply_all_to_dict(nested_path, cloned_dict, key)
+
+            return cloned_dict
+
+        elif isinstance(obj, list):
+            orig_list = obj
+            cloned_list = list(obj)
+
+            for i, _ in enumerate(orig_list):
+                nested_path = f"{path}[{i}]"
+                self.apply_all_to_list(nested_path, cloned_list, i)
+
+            return cloned_list
+
+
+def obj_matcher(dct: JsonDict, patches: Dict[Any, Any]) -> JsonDict:
+    patcher = ObjectPatcher.create(patches)
+    return patcher.rewrite(dct)
--- tests_unit/libtests/test_matchers.py
+++ tests_unit/libtests/test_matchers.py
@@ -1,36 +1,81 @@
-from libtests.matchers import dict_matcher
+from libtests.matchers import obj_matcher
+
+# single level dict
 
 
-def test_single_top_level_key() -> None:
-    fst = {"id": 123, "name": "James"}
-    snd = {"id": 123, "name": "Bill"}
+def test__single_level_dict__noop() -> None:
+    fst = {"id": 123, "first_name": "James", "last_name": "Joy"}
+    snd = {"id": 123, "first_name": "James", "last_name": "Joy"}
 
-    assert fst == dict_matcher(snd, name="James")
-
-
-def test_single_top_level_key__contains_dash__using_args() -> None:
-    fst = {"id": 123, "first-name": "James"}
-    snd = {"id": 123, "first-name": "Bill"}
-
-    assert fst == dict_matcher(snd, {"first-name": "James"})
+    assert fst == obj_matcher(snd, {})
 
 
-def test_single_top_level_key__contains_dash__using_kwargs() -> None:
-    fst = {"id": 123, "first-name": "James"}
-    snd = {"id": 123, "first-name": "Bill"}
+def test__single_level_dict__single_assignment() -> None:
+    fst = {"id": 123, "first_name": "James", "last_name": "Joy"}
+    snd = {"id": 123, "first_name": "Bill", "last_name": "Joy"}
 
-    assert fst == dict_matcher(snd, **{"first-name": "James"})
-
-
-def test_two_top_level_keys() -> None:
-    fst = {"id": 123, "name": "James"}
-    snd = {"id": 456, "name": "Bill"}
-
-    assert fst == dict_matcher(snd, id=123, name="James")
+    assert fst == obj_matcher(snd, {"= /first_name": "James"})
 
 
-def test_nested_key() -> None:
-    fst = {"level": 2, "person": {"id": 123, "name": "James"}}
-    snd = {"level": 2, "person": {"id": 123, "name": "Bill"}}
+def test__single_level_dict__multiple_assignments() -> None:
+    fst = {"id": 123, "first_name": "James", "last_name": "Joy"}
+    snd = {"id": 456, "first_name": "Bill", "last_name": "Joy"}
 
-    assert fst == dict_matcher(snd, {"person.name": "James"})
+    assert fst == obj_matcher(snd, {"= /id": 123, "= /first_name": "James"})
+
+
+def test__single_level_dict__single_delete() -> None:
+    fst = {"id": 123, "last_name": "Joy"}
+    snd = {"id": 123, "first_name": "Bill", "last_name": "Joy"}
+
+    assert fst == obj_matcher(snd, {"- /first_name": None})
+
+
+def test__single_level_dict__assign_and_delete() -> None:
+    fst = {"id": 123, "last_name": "Joy"}
+    snd = {"id": 456, "first_name": "Bill", "last_name": "Joy"}
+
+    assert fst == obj_matcher(snd, {"= /id": 123, "- /first_name": None})
+
+
+# single level list
+
+
+def test__single_level_list__noop() -> None:
+    fst = [1, 2, 3]
+    snd = [1, 2, 3]
+
+    assert fst == obj_matcher(snd, {})
+
+
+def test__single_level_list__single_delete() -> None:
+    fst = [1, 3]
+    snd = [1, 2, 3]
+
+    assert fst == obj_matcher(snd, {"- /[1]": None})
+
+
+def _test__single_level_list__multiple_deletions() -> None:
+    fst = [2]
+    snd = [1, 2, 3]
+
+    assert fst == obj_matcher(snd, {"- /[2]": None, "- /[0]": None})
+
+    # assert fst == dict_matcher(snd, {"= /persons[*]/name": None})
+    # assert fst == dict_matcher(snd, {"= /persons[2]/name": None})
+    # assert fst == dict_matcher(snd, {"= /persons[-1]/name": None})
+    # assert fst == dict_matcher(snd, {"- /persons[*]/name": None})
+
+
+# def test_two_top_level_keys() -> None:
+#     fst = {"id": 123, "name": "James"}
+#     snd = {"id": 456, "name": "Bill"}
+
+#     assert fst == dict_matcher(snd, id=123, name="James")
+
+
+# def test_nested_key() -> None:
+#     fst = {"level": 2, "person": {"id": 123, "name": "James"}}
+#     snd = {"level": 2, "person": {"id": 123, "name": "Bill"}}
+
+#     assert fst == dict_matcher(snd, {"person.name": "James"})
