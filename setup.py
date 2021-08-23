a0c33f73fbaf5e99dfa9fea2924bfc52bafb337d
Martin Matusiak <numerodix@gmail.com>
2021-08-23 11:07:12 +1000

    working packaging test

--- setup.py
+++ setup.py
@@ -5,22 +5,22 @@ import libddog
 setup(
     name="libddog",
     version=libddog.__version__,
-    description="Datadog dashboard automation tool",
+    description="Datadog automation tool",
     author="Martin Matusiak",
     author_email="martin.matusiak@nearmap.com",
     url="https://github.com/nearmap/libddog",
-    packages=find_packages(".", exclude=("tests_*",)),
+    packages=find_packages('.', exclude=('libtests', 'testdata', 'tests_*',)),
     package_dir={"": "."},
     package_data={
         "libddog": ["py.typed"],
         "libddog.parsing": ["grammar.txt"]
     },
+    # NOTE: also cross check with requirements.txt
     install_requires=[
-        "click==8.0.1",
-        "datadog==0.41.0",
-        "humanize==3.6.0",
-        "parsimonious==0.8.1",
-        "python-dateutil==2.8.1",
+        "click>=8.0.0",
+        "datadog>=0.41.0",
+        "parsimonious>=0.8.0",
+        "python-dateutil>=2.0.0",
     ],
     # don't install as zipped egg
     zip_safe=False,
