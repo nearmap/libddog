a13bec90de8aa044d843243043ea0d8cd57a9f5f
Martin <83994886+martinm-nm@users.noreply.github.com>
2021-09-22 13:59:52 +1000

    Update setup.py with a license and a long description (#43)
    * add license field to setup.py
    
    * add long_description in setup.py
    
    * update maintainer guide
    
    * bump version to -0.0.7a1
    
    * add abbreviated readme for pypi
    
    Co-authored-by: Martin Matusiak <numerodix@gmail.com>

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 from setuptools import find_packages, setup
+from pathlib import Path
 
 import libddog
+
+proj_dir = Path(__file__).parent
+long_desc = (proj_dir / "README-PYPI.md").read_text()
 
 setup(
     name="libddog",
     version=libddog.__version__,
     description="Datadog automation tool",
+    long_description=long_desc,
+    long_description_content_type='text/markdown',
     author="Martin Matusiak",
     author_email="martin.matusiak@nearmap.com",
     url="https://github.com/nearmap/libddog",
+    license="MIT",
     packages=find_packages('.', exclude=('libtests', 'testdata', 'tests_*',)),
     package_dir={"": "."},
     package_data={
