3ffb5f60f3d1aaaf6a797cb6014f2640e459fb9a
Martin Matusiak <numerodix@gmail.com>
2021-11-12 11:30:28 +1100

    remove dependency on datadog

--- setup.py
+++ setup.py
@@ -25,7 +25,6 @@ setup(
     # NOTE: also cross check with requirements.txt
     install_requires=[
         "click>=8.0.0",
-        "datadog>=0.41.0",
         "packaging>=21.0",
         "parsimonious>=0.8.0",
         "python-dateutil>=2.0.0",
